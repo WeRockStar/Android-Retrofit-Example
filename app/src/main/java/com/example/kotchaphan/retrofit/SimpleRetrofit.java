2f60dbee2b92a7bf214fbd6578f0aebd9d993550
WeRockStar <vr_freestyle@outlook.com>
2015-09-04 01:24:10 +0700

    add new routing id for retrofit interface

--- app/src/main/java/com/example/kotchaphan/retrofit/SimpleRetrofit.java
+++ app/src/main/java/com/example/kotchaphan/retrofit/SimpleRetrofit.java
@@ -1,6 +1,7 @@
 package com.example.kotchaphan.retrofit;
 
 import retrofit.http.GET;
+import retrofit.http.Path;
 
 /**
  * Created by Kotchaphan on 4/9/2558.
@@ -9,4 +10,7 @@ public interface SimpleRetrofit {
 
     @GET("/shots/21603")
     Shot getShot();
+
+    @GET("shots/{id}")
+    Shot getShotById(@Path("id") int id);
 }
