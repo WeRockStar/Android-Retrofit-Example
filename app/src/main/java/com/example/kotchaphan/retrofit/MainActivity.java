ebd486996ae957b889a628dac1fc30a64ea18fe8
WeRockStar <vr_freestyle@outlook.com>
2015-09-04 01:21:37 +0700

    implement async task

--- app/src/main/java/com/example/kotchaphan/retrofit/MainActivity.java
+++ app/src/main/java/com/example/kotchaphan/retrofit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kotchaphan.retrofit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
@@ -13,16 +14,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RestAdapter restAdapter = new RestAdapter.Builder()
-                .setEndpoint("http://api.dribbble.com/")
-                .build();
-
-        //SimpleRetrofit interface
-        SimpleRetrofit retrofit = restAdapter.create(SimpleRetrofit.class);
-        Shot shot = retrofit.getShot();
-
-        Toast.makeText(getApplicationContext(), "Title : " + shot.getTitle() + " URL : " + shot.getUrl(), Toast.LENGTH_SHORT).show();
 
         new HttpAsyncTask().execute();
+    }
+
+    class HttpAsyncTask extends AsyncTask<Void, Void, Shot> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Shot shot) {
+            Toast.makeText(getApplicationContext(), "Title : " + shot.getTitle() + " URL : " + shot.getUrl(), Toast.LENGTH_LONG).show();
+
+            super.onPostExecute(shot);
+        }
+
+        @Override
+        protected Shot doInBackground(Void... params) {
+            RestAdapter restAdapter = new RestAdapter.Builder()
+                    .setEndpoint("http://api.dribbble.com/")
+                    .build();
+
+            //SimpleRetrofit interface
+            SimpleRetrofit retrofit = restAdapter.create(SimpleRetrofit.class);
+            Shot shot = retrofit.getShot();
+            return shot;
+        }
     }
 }
